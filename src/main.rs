9ba7474458bd027251967961e02e3e44cb4ed578
Gabriel França <gabrielgfranca7@proton.me>
2026-06-10 21:06:52 -0300

    feat: Initialize application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+    process,
+};
+
+use todo_list_cli::Config;
+
+fn main() 
+{
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(err) = todo_list_cli::run(config) {
+        eprintln!("Aplication error: {err}");
+        process::exit(1);
+    }
 }
+
